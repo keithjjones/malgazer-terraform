30812a41596d47989ec83ceba8ea0c5d4e9ccdb6
Keith J. Jones <keith@keithjjones.com>
2018-07-19 12:40:13 -0400

    Improved script.

--- malgazer-training/scripts/build_training.sh
+++ malgazer-training/scripts/build_training.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
-mkdir /mnt/data
-cd /mnt/data
-cp -R /mnt/virustotal/RWE .
-cp -R /mnt/virustotal/GIST .
+
+# Get the source code...
 mkdir -p /Source
 cd /Source
 git clone https://github.com/keithjjones/malgazer.git
 cd malgazer
 pip install -r requirements.txt
+
+# Copy the data over...
+# mkdir /mnt/data
+# cd /mnt/data
+# cp -R /mnt/virustotal/RWE .
+# cp -R /mnt/virustotal/GIST .

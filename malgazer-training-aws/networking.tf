e6700b0cf68ecb10e2e0b03cc876ae8a7d948551
Keith J. Jones <keith@keithjjones.com>
2018-07-30 12:49:53 -0400

    Added home IP.

--- malgazer-training-aws/networking.tf
+++ malgazer-training-aws/networking.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "allow_ssh" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-    # cidr_blocks = ["${var.home_ip}/32"]
+    # cidr_blocks     = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.home_ip}/32"]
   }
 
   egress {
